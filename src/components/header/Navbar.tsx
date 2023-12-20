27a1539d5149fef2c08fb3d0522bbe950df1ced0
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-12-21 02:42:09 +0300

    Add hero section

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -9,9 +9,9 @@ const Navbar = () => {
   };
   return (
     <header>
-      <nav className="h-16 border flex justify-between items-center relative px-4">
+      <nav className="h-16 flex justify-between items-center relative px-4">
         <div>
-          <NavLink to="/">Logo</NavLink>
+          <NavLink to="/" className="flex text-3xl items-center">D <span className="text-[#f01d52]">omino</span> </NavLink>
         </div>
         <button
           type="button"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import heroImage from '../assets/images/transparent-light-bulb.png'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="h-[calc(100vh-64px)] bg-[#f01d52] flex items-center justify-center p-4">
+      <img src={heroImage} alt="hero image" className="hero-image" />
+      <div className="h-full flex flex-col justify-center items-center absolute">
+        <h1 className="hero-text text-6xl text-white font-bold p-4 text-center">GET THE RESULTS YOU WANT</h1>
+      </div>
+    </section>
   )
 }
 
